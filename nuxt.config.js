fc60499e2d3acf58d9b8f0ddb78046501b1871e7
cabbagekobe <circlekdeath@gmail.com>
2021-09-10 02:09:00 +0900

    多少のアップデート

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,13 +4,22 @@ export default {
   head: {
     title: 'cabbagekobe.info',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'ja',
+      prefix: 'og: http://ogp.me/ns#'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: 'cabbagekobe.infoです。' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'cabbagekobe.info' },
+      { hid: 'og:type', property: 'og:type', content: 'article' },
+      { hid: 'og:url', property: 'og:url', content: 'https://cabbagekobe.info' },
+      { hid: 'og:title', property: 'og:title', content: 'cabbagekobe.info' },
+      { hid: 'og:description', property: 'og:description', content: 'cabbagekobe.infoです。' },
+      { hid: 'og:image', property: 'og:image', content: 'https://cabbagekobe.info/ogp.jpg' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:url', content: 'https://cabbagekobe.info' },
+      { name: 'twitter:title', content: 'cabbagekobe.info'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -28,7 +37,7 @@ export default {
   },
 
   css: [
-    '@/assets/css/main.scss',
+    { src: '@/assets/css/main.scss', lang: 'scss' },
   ],
 
   plugins: [
